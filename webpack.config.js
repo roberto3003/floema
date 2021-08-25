222ce69c5f4d96220130722a386e11eba725e716
roberto3003 <roberto3003@riseup.net>
2021-08-25 21:42:44 +0700

    images videos

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,10 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
 const dirApp = path.join(__dirname, 'app')
+const dirImages = path.join(__dirname, 'images')
 const dirShared = path.join(__dirname, 'shared')
 const dirStyles = path.join(__dirname, 'styles')
+const dirVideos = path.join(__dirname, 'videos')
 const dirNode = 'node_modules'
 
 module.exports = {
@@ -22,8 +24,10 @@ module.exports = {
   resolve: {
 	modules: [
 	  dirApp,
+	  dirImages,
 	  dirStyles,
 	  dirShared,
+	  dirVideos,
 	  dirNode
 	]
   },
@@ -43,5 +47,36 @@ new MiniCssExtractPlugin({
   filename: '[name].css',
   chunkFilename: '[id].css'
 })
+  ],
+module: {
+  rules: [
+	{
+	  test: /\.js$/,
+	  use: {
+		loader: 'babel-loader'
+	  },
+	  {
+		test: /\.scss$/,
+		use: [
+		  loader: MiniCssExtractPlugin.loader,
+		  options: {
+			publicPath: ''
+		  }
+		},
+		{
+		  loader: 'css-loader'
+		},
+		{
+		  loader: 'postcss-loader'
+		},
+		{
+		  loader: 'sass-loader'
+		}
+	  ]
+	},
+	{
+	  test: /\.(jpe?g|png|gif|svg|wof2?|fnt|webp)$/,
+	}
   ]
+	}
 }
