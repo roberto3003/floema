b7652338a59eaccca74fe5db20b1a5df126c63d7
roberto3003 <roberto3003@riseup.net>
2021-08-25 20:40:34 +0700

    webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,39 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
 const dirApp = path.join(__dirname, 'app')
-const dirAssets = path.join(__dirname, 'assets')
+const dirShared = path.join(__dirname, 'shared')
 const dirStyles = path.join(__dirname, 'styles')
+const dirNode = 'node_modules'
 
 module.exports = {
   entry: [
-	path.join(dirApp, 'index.js')
+	path.join(dirApp, 'index.js'),
+	path.join(dirStyles, 'index.scss')
+  ],
+
+  resolve: {
+	modules: [
+	  dirApp,
+	  dirStyles,
+	  dirShared,
+	  dirNode
+	]
+  },
+  plugins: [
+	new webpack.DefinePlugin({
+	  IS_DEVELOPMENT
+	})
+	new CopyWebpackPlugin({
+	  patterns: [
+		{
+		  from: './shared',
+		  to: ''
+		}
+	  ]
+	})
+new MiniCssExtractPlugin({
+  filename: '[name].css',
+  chunkFilename: '[id].css'
+})
   ]
 }
